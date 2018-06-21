6a84a9bcd9f00fc4bb022e8d513fcf1b1ec1946c
gongfukangEE <gongfukang@outlook.com>
2018-06-21 10:31:08 +0800

    更新 leetcode

--- src/Algorithm/LeetCode/T198/Solution_2.java
+++ src/Algorithm/LeetCode/T198/Solution_2.java
@@ -10,8 +10,17 @@ public class Solution_2 {
         int n = nums.length;
         if (n == 0)
             return 0;
+        // memo[i] 表示考虑抢劫 nums[ 1,...,n-1] 所能获得的最大收益
         int[] memo = new int[n];
+        // memo[n - 1] 表示考虑抢劫 nums[n-1, n-1] 所能获得的最大收益，由于仅包含一座房子，则必定偷取
         memo[n - 1] = nums[n - 1];
-        for (int i = )
+        for (int i = n - 2; i >= 0; i--) {
+            // 每次都在求解 memo[i]
+            for (int j = i; j < n; j++) {
+                // 考虑偷取 nums[j] 房子
+                memo[i] = Math.max(memo[i], nums[j] + (j + 2 < n ? memo[j + 2] : 0));
+            }
+        }
+        return memo[0];
     }
 }
